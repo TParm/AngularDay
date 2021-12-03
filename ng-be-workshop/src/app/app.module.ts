54af6a852b5d7721bfed4686d93a8f4d13c85bf0
ChrisAzee <thibault.parmentier@hotmail.com>
2021-12-03 13:44:40 +0100

    adding navigationbar

--- ng-be-workshop/src/app/app.module.ts
+++ ng-be-workshop/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 
 import { BooksModule } from './books/books.module';
 import { AboutModule } from './about/about.module';
+import { NavigationComponent } from './navigation/navigation.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavigationComponent,
   ],
   imports: [
     BrowserModule,
